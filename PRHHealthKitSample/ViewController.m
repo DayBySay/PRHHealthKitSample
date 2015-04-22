9b932009766614102f854a80fd1dd7b0d81477b6
Takayuki Sei <takayuki_sei@voyagegroup.com>
2015-04-22 23:45:26 +0900

    HealthKitを使って毎日の歩数のStatisticsCollectionを取得するところまで

--- PRHHealthKitSample/ViewController.m
+++ PRHHealthKitSample/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <NSDate+Escort.h>
+@import HealthKit;
 
 @interface ViewController ()
+
+@property (nonatomic) HKHealthStore *healthStore;
 
 @end
 
@@ -16,12 +20,54 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.healthStore = [[HKHealthStore alloc] init];
+    
+    if ([HKHealthStore isHealthDataAvailable]) {
+        NSSet *writeDataTypes = [self dataTypesToWrite];
+        NSSet *readDataTypes = [self dataTypesToRead];
+        
+        [self.healthStore requestAuthorizationToShareTypes:writeDataTypes readTypes:readDataTypes completion:^(BOOL success, NSError *error) {
+            if (!success) {
+                NSLog(@"You didn't allow HealthKit to access these read/write data types. In your app, try to handle this error gracefully when a user decides not to provide access. The error was: %@. If you're using a simulator, try it on a device.", error);
+                
+                return;
+            }
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                // Update the user interface based on the current user's health information.
+                [self updateUsersStepCountLabel];
+            });
+        }];
+    }
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)updateUsersStepCountLabel {
+    HKQuantityType *stepCountType = [HKQuantityType quantityTypeForIdentifier:HKQuantityTypeIdentifierStepCount];
+    NSDateComponents *oneDateComponent = [[NSDateComponents alloc] init];
+    oneDateComponent.day = 1;
+    HKStatisticsCollectionQuery *query = [[HKStatisticsCollectionQuery alloc] initWithQuantityType:stepCountType
+                                                                            quantitySamplePredicate:nil
+                                                                                            options:HKStatisticsOptionCumulativeSum
+                                                                                         anchorDate:[[NSDate date] dateAtStartOfMonth]
+                                                                                 intervalComponents:oneDateComponent];
+    [query setInitialResultsHandler:^(HKStatisticsCollectionQuery *query, HKStatisticsCollection *collection, NSError *error) {
+       [collection.statistics enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+           HKStatistics *statistics = obj;
+           NSLog(@" \n start:%@ \n end:%@ \n count:%@ \n", statistics.startDate, statistics.endDate, statistics.sumQuantity);
+       }];
+    }];
+    [self.healthStore executeQuery:query];
+}
+
+- (NSSet *)dataTypesToWrite {
+    return nil;
+}
+
+// Returns the types of data that Fit wishes to read from HealthKit.
+- (NSSet *)dataTypesToRead {
+    HKQuantityType *stepCount = [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierStepCount];
+    
+    return [NSSet setWithObjects:stepCount, nil];
 }
 
 @end
